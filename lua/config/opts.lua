d4885341a35fecc4b8e4555a9d76337633cd42ec
verbenaidk <verbenaidk@gmail.com>
2025-10-29 13:14:30 -0300

    Make selecting and highlighting more readable
    Contrast was bloody awful

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -10,3 +10,7 @@ vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
 vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
 -- tab changes
+
+-- Highlight color fix
+
+vim.api.nvim_set_hl(0, "Visual", { bold = true, fg = "#DCC8FF", bg = "#131218", standout = true })
