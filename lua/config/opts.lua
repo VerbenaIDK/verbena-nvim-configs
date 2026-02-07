3c4954eadba890f4dc3542b20a8b3dbbb2d8c907
verbenaidk <verbenaidk@gmail.com>
2026-02-07 06:03:44 -0300

    New theme and indents uwu owo rawr x3

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -6,9 +6,9 @@ vim.opt.clipboard = "unnamedplus" -- Throw yank/cut to clipboard, needs wl-clipb
 -- tab changes
 vim.opt.smartindent = true
 vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
-vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
 -- tab changes
 
 -- Highlight color fix
