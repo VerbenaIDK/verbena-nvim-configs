b37ba60dd265604a96cacdf5c70c984d05716601
verbenaidk <verbenacomms@gmail.com>
2026-02-08 15:48:51 -0300

    Some reversions, some changes Mostly related to terminal usage meow

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -15,3 +15,24 @@ vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while perfor
 -- Highlight color fix for witchhazel
 
 -- vim.api.nvim_set_hl(0, "Visual", { bold = true, fg = "#DCC8FF", bg = "#131218", standout = true })
+
+-- toggleterm things
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  --vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  --vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  --vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  --vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  --vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  --vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+  vim.keymap.set('t', '<leader>w', [[<C-\><C-n><leader>w]])
+  vim.keymap.set('t', '<leader><left>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<leader><right>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<leader><up>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<leader><down>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<leader>q', [[<Cmd>ToggleTerm<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,6 +3,8 @@ return {
     version = "*",
     config = true,
     keys = {
-        {"<leader>t", "<cmd>ToggleTerm direction=float name=Terminal<cr>", desc = "Toggle floating terminal"}
+        {"<leader>t", "<cmd>ToggleTerm direction=float name=Terminal<cr>", desc = "Toggle floating terminal"},
+        {"<leader>ht", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Split terminal horizontal"},
+        {"<leader>vt", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Split terminal vertical"}
     }
 }
